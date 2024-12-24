f40b1586fce3a01166342e1f33688fe6058633b8
Filipp Vasilev <ampa1r.pl@gmail.com>
2024-12-24 21:26:41 +0400

    Day 2 Part 2: Go solution

--- days/1/go/day-1.go
+++ days/1/go/day-1.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-func main() {
+func partOne() int {
 	absPath, _ := filepath.Abs("days/1/input-1.txt")
 
 	inputData, err := os.ReadFile(absPath)
@@ -47,5 +47,51 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Similarity score is %d \n", distance)
+	return distance
+}
+
+func partTwo() int {
+
+	absPath, _ := filepath.Abs("days/1/input-1.txt")
+
+	inputData, err := os.ReadFile(absPath)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(inputData), "\n")
+
+	leftArray := []int{}
+
+	itemsMap := make(map[int]int)
+
+	for i := 0; i < len(lines); i++ {
+		parts := strings.Split(lines[i], "   ")
+		if len(parts) != 2 {
+			continue
+		}
+
+		left, _ := strconv.Atoi(parts[0])
+		right, _ := strconv.Atoi(parts[1])
+
+		leftArray = append(leftArray, left)
+		itemsMap[right] += 1
+	}
+
+	var distance int = 0
+
+	for i := 0; i < len(leftArray); i++ {
+		distance += leftArray[i] * itemsMap[leftArray[i]]
+	}
+
+	return distance
+}
+
+func main() {
+
+	partOneDistance := partOne()
+	fmt.Printf("[Part 1] Similarity score is %d \n", partOneDistance)
+
+	partTwoDistance := partTwo()
+	fmt.Printf("[Part 2] Similarity score is %d \n", partTwoDistance)
 }
